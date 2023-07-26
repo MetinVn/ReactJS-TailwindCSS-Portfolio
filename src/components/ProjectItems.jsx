70160b457ea5d5bfb446b90ef3d21003260fd2b9
Metin <misaxanli@gmail.com>
2023-07-26 08:54:20 +0400

    duration

--- src/components/ProjectItems.jsx
+++ src/components/ProjectItems.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProjectItems = ({img,title,link}) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
-        <img src={img} alt={title} className='rounded-xl group-hover:opacity-10'/>
+        <img src={img} alt={title} className='rounded-xl group-hover:opacity-10 ease-in-out duration-300'/>
         <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-md md:text-2xl font-bold text-white text-center mb-[10px]'>
                 {title}
