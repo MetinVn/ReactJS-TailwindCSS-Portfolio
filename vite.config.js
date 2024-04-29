b2ce5d0c1e5ec04833dce4431ebfe77a18713a41
Metin <misaxanli@gmail.com>
2024-04-29 18:33:38 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   base: "/ReactJS-TailwindCSS-Portfolio/",
-  plugins: [react(), reactRefresh()],
-  resolve: {
-    alias: {
-      "/@/": `${path.resolve(new URL(import.meta.url).pathname, "../../src")}/`,
-    },
-  },
+  plugins: [react()],
 });
