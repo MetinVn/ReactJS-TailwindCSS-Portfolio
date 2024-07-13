8ab8fce1706dd8188e258f0df668c1d8fee07d2d
Metin <misaxanli@gmail.com>
2024-07-13 06:49:15 +0300

    image-carousel-for-dark-mode

--- src/components/ProjectItems.jsx
+++ src/components/ProjectItems.jsx
@@ -9,57 +9,97 @@ const ProjectItems = ({
   description,
   techStack,
 }) => {
-  const [imageLoaded, setImageLoaded] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageLoaded, setImageLoaded] = useState(Array(img.length).fill(false));
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? img.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === img.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const handleImageLoad = (index) => {
+    setImageLoaded((prevLoaded) => {
+      const newLoaded = [...prevLoaded];
+      newLoaded[index] = true;
+      return newLoaded;
+    });
+  };
 
   return (
-    <div className="bg-white overflow-hidden border rounded-lg border-transparent hover:border-[#008080]">
-      <div style={{ position: "relative", width: "100%", height: "100%" }}>
-        <img
-          src={blurImg}
-          alt={title}
-          className="w-full h-full object-cover"
-          style={{ filter: "blur(10px)", opacity: imageLoaded ? 0 : 1 }}
-        />
-        <img
-          src={img}
-          alt={title}
-          className="w-full h-full object-cover"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            opacity: imageLoaded ? 1 : 0,
-            transition: "opacity 0.5s ease-in-out",
-          }}
-          onLoad={() => setImageLoaded(true)}
-        />
+    <div className="bg-white overflow-hidden border rounded-lg border-transparent hover:border-[#008080] relative">
+      <div className="relative" style={{ paddingBottom: "56.25%" }}>
+        {blurImg.map((blurImage, index) => (
+          <img
+            key={`blur-${index}`}
+            src={blurImage}
+            alt={title}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            style={{
+              filter: "blur(10px)",
+              opacity: currentIndex === index && !imageLoaded[index] ? 1 : 0,
+              transition: "opacity 0.5s ease-in-out",
+            }}
+          />
+        ))}
+        {img.map((image, index) => (
+          <img
+            key={`img-${index}`}
+            src={image}
+            alt={title}
+            className="absolute top-0 left-0 w-full h-full object-contain"
+            style={{
+              opacity: currentIndex === index && imageLoaded[index] ? 1 : 0,
+              transition: "opacity 0.5s ease-in-out",
+            }}
+            onLoad={() => handleImageLoad(index)}
+          />
+        ))}
       </div>
-      <div className="p-4">
-        <p className="text-xl font-semibold mb-2 text-[#9966CC]">{title}</p>
-        <h1 className="text-[15px] leading-tight font-semibold mb-2 text-[#9966CC]">
-          Technologies/Skills Used: {techStack}
-        </h1>
-        <h3 className="text-gray-700 mb-4 text-xs sm:text-sm">
-          <span className="font-semibold text-xs text-[#9966CC]">
-            Description:{" "}
-          </span>
-          {description}
-        </h3>
-        <div className="flex gap-1">
-          <a
-            href={linkProject || undefined}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-xs sm:text-base px-2 py-1 md:px-4 md:py-2 bg-[#9966CC] text-white hover:bg-[#008080] transition duration-300 ease-in-out rounded">
-            View Project
-          </a>
-          <a
-            href={linkSource}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block text-xs sm:text-base px-2 py-1 md:px-4 md:py-2 border border-[#9966CC] rounded hover:border-[#008080] transition duration-300 ease-in-out">
-            {"Source Code </>"}
-          </a>
+
+      <div className="p-4 flex flex-col items-start">
+        <h3 className="text-xl font-bold mb-2 text-[#9966CC]">{title}</h3>
+        <p className="text-gray-700 mb-4">{description}</p>
+        <p className="text-sm  mb-4 text-[#008080]">{techStack}</p>
+        <div className="flex items-center space-x-4 mt-4 md:mt-0">
+          {linkProject && (
+            <a
+              href={linkProject}
+              className=" inline-block text-xs sm:text-base px-2 py-1 md:px-4 md:py-2 bg-[#9966CC] text-white hover:bg-[#008080] transition duration-300 ease-in-out rounded"
+              target="_blank"
+              rel="noopener noreferrer">
+              Live Project
+            </a>
+          )}
+          {linkSource && (
+            <a
+              href={linkSource}
+              className="inline-block text-xs sm:text-base px-2 py-1 md:px-4 md:py-2 border border-[#9966CC] rounded hover:border-[#008080] transition duration-300 ease-in-out"
+              target="_blank"
+              rel="noopener noreferrer">
+              Source Code
+            </a>
+          )}
+          {(img.length > 1 || blurImg.length > 1) && (
+            <div className="flex gap-4">
+              <button
+                onClick={handlePrevClick}
+                className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white p-2 rounded-full shadow-md transition duration-300 ease-in-out w-10 h-10 flex items-center justify-center opacity-75">
+                {"<"}
+              </button>
+              <button
+                onClick={handleNextClick}
+                className="bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white p-2 rounded-full shadow-md transition duration-300 ease-in-out w-10 h-10 flex items-center justify-center opacity-75">
+                {">"}
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,54 @@
 import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import ProjectItems from "./ProjectItems";
-import Summary from "./Summary";
-import Recipe from "../images/Recipe.png";
-import Movie from "../images/MovieApp.png";
-import Weather from "../images/Capture.png";
-import Translator from "../images/Translator.jpeg";
-import Food from "../images/food.png";
-import Product from "../images/productManage.png";
-import USA from "../images/us.jpg";
 import blurRecipe from "../images/reduced/RecipeApp.jpg";
+import blurRecipeDark from "../images/reduced/Recipe.jpg";
+import RecipeDark from "../images/Recipe.png";
+import Recipe from "../images/RecipeApp.png";
 import blurMovie from "../images/reduced/MovieApp.jpg";
 import blurWeather from "../images/reduced/Capture.jpg";
 import blurTranslator from "../images/reduced/Translator.jpg";
 import blurFood from "../images/reduced/food.jpg";
 import blurProduct from "../images/reduced/productManage.jpg";
 import blurUSA from "../images/reduced/us.jpg";
+import Movie from "../images/MovieApp.png";
+import Weather from "../images/Capture.png";
+import Translator from "../images/Translator.jpeg";
+import Food from "../images/food.png";
+import Product from "../images/productManage.png";
+import USA from "../images/us.jpg";
+import Summary from "./Summary";
+import blurAuthorsPage from "../images/reduced/AuthorsPage.jpg";
+import blurAuthorsPageDark from "../images/reduced/AuthorsPageDark.jpg";
+import blurBooksPage from "../images/reduced/BooksPage.jpg";
+import blurBooksPageDark from "../images/reduced/BooksPageDark.jpg";
+import AuthorsPage from "../images/AuthorsPage.jpg";
+import AuthorsPageDark from "../images/AuthorsPageDark.jpg";
+import BooksPage from "../images/BooksPage.jpg";
+import BooksPageDark from "../images/BooksPageDark.jpg";
 const Projects = () => {
   const projects = [
+    {
+      title: "Open Library Project",
+      description:
+        "I built this project using React.js, integrating Open Library APIs for searching books and authors. It features Material UI components for design and TailwindCSS for additional styling. Data storage in IndexDB optimizes performance by minimizing API requests. I enhanced user interaction with features like Abort Controller for managing and canceling API requests. Plus, I implemented sorting algorithms for sorting author and book titles.",
+      img: [AuthorsPage, AuthorsPageDark, BooksPage, BooksPageDark],
+      blurImg: [
+        blurAuthorsPage,
+        blurAuthorsPageDark,
+        blurBooksPage,
+        blurBooksPageDark,
+      ],
+      linkProject: "https://metinvn.github.io/Open-Library-Search-API/",
+      linkSource: "https://github.com/MetinVn/Open-Library-Search-API",
+      techStack: "React, MUI Material, TailwindCSS, React Hooks",
+    },
     {
       title: "Product Management",
       description:
         "The Product Management app offers a robust set of functionalities, including pagination, user registration and login capabilities for secure access. Users can: filter products by category names,add product to database, search product with debounce optimized functionality.",
-      img: Product,
-      blurImg: blurProduct,
+      img: [Product],
+      blurImg: [blurProduct],
       linkSource: "https://github.com/MetinVn/Product-Management",
       techStack:
         "React, Semantic UI, Formik, Yup, Redux, PostgreSQL, Java, Swagger, Spring Boot",
@@ -32,8 +57,8 @@ const Projects = () => {
       title: "Weather App",
       description:
         "Real-Time Weather Data: Fetches real-time weather information for various locations. Search Functionality: Allows users to search for weather information by city name.",
-      img: Weather,
-      blurImg: blurWeather,
+      img: [Weather],
+      blurImg: [blurWeather],
       linkProject: "https://metinvn.github.io/Weather/",
       linkSource: "https://github.com/MetinVn/Weather",
       techStack: "React, Tailwind CSS, Framer Motion, Debounce",
@@ -42,8 +67,8 @@ const Projects = () => {
       title: "Individual Armament",
       description:
         "Individual armament project retrieves USA crime rates across all states. It includes an interactive map of the USA, allowing users to click on states and access specific crime data. The project leverages GeoJson for data retrieval, also integrates React Charts for data visualization, and utilizes Framer Motion for smooth and interactive animations.",
-      img: USA,
-      blurImg: blurUSA,
+      img: [USA],
+      blurImg: [blurUSA],
       linkProject: "https://metinvn.github.io/Bireysel-Silahlanma/",
       linkSource: "https://github.com/MetinVn/Bireysel-Silahlanma",
       techStack: "React, React charts, Framer-Motion, TailwindCSS, Axios",
@@ -52,8 +77,8 @@ const Projects = () => {
       title: "E-Commerce",
       description:
         "My demo version of e-commerce website for popular food in Turkey called Orcik. Users can order the food through filling the form and after that, they can track their cargo details with the help of RapidApi call and the key we send them.",
-      img: Food,
-      blurImg: blurFood,
+      img: [Food],
+      blurImg: [blurFood],
       linkProject: "https://metinvn.github.io/Orcik-Website/",
       linkSource: "https://github.com/MetinVn/Orcik-Website",
       techStack: "React, TailwindCSS, Framer-Motion",
@@ -62,8 +87,8 @@ const Projects = () => {
       title: "Movie App",
       description:
         "App utilizes RapidAPI to fetch data and dynamically visualizes movie charts based on user search input. Users can explore movie details including ,release date ,original language and overview.",
-      img: Movie,
-      blurImg: blurMovie,
+      img: [Movie],
+      blurImg: [blurMovie],
       linkProject: "https://metinvn.github.io/Movie-App/",
       linkSource: "https://github.com/MetinVn/Movie-App",
       techStack: "HTML, CSS, JavaScript",
@@ -72,8 +97,8 @@ const Projects = () => {
       title: "Translator",
       description:
         "Lifesaver translation app. It makes switching between Russian, German, Spanish, Turkish, Italian, and Azerbaijani a breeze, helping users communicate effectively and explore new languages with ease.",
-      img: Translator,
-      blurImg: blurTranslator,
+      img: [Translator],
+      blurImg: [blurTranslator],
       linkProject: "https://metinvn.github.io/Translator/",
       linkSource: "https://github.com/MetinVn/Translator",
       techStack: "React, React spinners, TailwindCSS, Axios",
@@ -82,8 +107,8 @@ const Projects = () => {
       title: "Recipe App",
       description:
         "Recipe app utilizes meal api that retrieves meal ingredients, instructions and measures.Users can also save meals to favorites for later.",
-      img: Recipe,
-      blurImg: blurRecipe,
+      img: [Recipe, RecipeDark],
+      blurImg: [blurRecipe, blurRecipeDark],
       linkProject: "https://metinvn.github.io/Recipe-App/recipe.html",
       linkSource: "https://github.com/MetinVn/Recipe-App",
       techStack: "HTML, CSS, JavaScript",
@@ -126,7 +151,7 @@ const Projects = () => {
     <>
       <div className="w-full h-2 bg-gradient-to-b from-black/30 to-white"></div>
       <div className="w-full">
-        <div className="max-w-[1340px] mx-auto md:pl-20 p-4 py-16">
+        <div className="max-w-[1340px] mx-auto p-4 py-16">
           <Summary />
           <div className="container mx-auto py-8">
             <h1 className="text-3xl font-bold text-center mb-8 text-[#9966CC]">
