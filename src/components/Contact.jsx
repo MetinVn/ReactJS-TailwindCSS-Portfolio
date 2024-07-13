9dcee292b8fbae81147d23d5e5d6b1e5257fcf43
Metin <misaxanli@gmail.com>
2024-07-14 00:19:26 +0300

    SimpleAnalyticsBadge

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,15 +34,36 @@ const Contact = () => {
             <h3 className="text-lg font-bold mb-2">Download CV</h3>
             <p className="mb-4">
               {`If you're interested in learning more about my qualifications and
-              experience, feel free to download my CV below:`}
+    experience, feel free to download my CV below:`}
             </p>
-            <a
-              href={pdf}
-              download
-              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
-              Download CV
-            </a>
+            <div className="flex justify-between items-center">
+              <a
+                href={pdf}
+                download
+                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300">
+                Download CV
+              </a>
+              <a
+                href="https://dashboard.simpleanalytics.com/?utm_source=metinvn.github.io&utm_content=badge"
+                referrerPolicy="origin"
+                target="_blank"
+                className="ml-4">
+                <picture>
+                  <source
+                    srcSet="https://simpleanalyticsbadges.com/metinvn.github.io?mode=dark"
+                    media="(prefers-color-scheme: dark)"
+                  />
+                  <img
+                    src="https://simpleanalyticsbadges.com/metinvn.github.io?mode=light"
+                    loading="lazy"
+                    referrerPolicy="no-referrer"
+                    crossOrigin="anonymous"
+                  />
+                </picture>
+              </a>
+            </div>
           </div>
+
           <hr className="border-t border-gray-700 my-6" />
           <p className="text-center text-sm">
             &copy; 2024 Matin Isakhanli. All rights reserved.
